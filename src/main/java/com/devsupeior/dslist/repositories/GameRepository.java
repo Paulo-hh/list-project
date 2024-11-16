3e284f1581f442abc70eec8e41c6911007bcba48
Paulo Hoff <paulohhoff@gmail.com>
2024-11-16 19:17:34 -0300

    Homolog ok

--- src/main/java/com/devsupeior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsupeior/dslist/repositories/GameRepository.java
@@ -11,7 +11,7 @@ import com.devsupeior.dslist.projections.GameMinProjection;
 public interface GameRepository extends JpaRepository<Game, Long> {
 	
 	@Query(nativeQuery = true, value = """
-			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 				tb_game.short_description AS shortDescription, tb_belonging.position
 			FROM tb_game, tb_belonging
 			WHERE tb_game.id = tb_belonging.game_id and tb_belonging.list_id = :listId
